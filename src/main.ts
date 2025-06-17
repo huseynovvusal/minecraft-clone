6955f3fcd30de6e8d473b04090e7be3d7a8bbaa6
Vusal Huseynov <vslh333222111@gmail.com>
2025-06-17 21:53:31 +0400

    fix: adjust indentation for better readability in main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,23 +11,23 @@ TextureManager.getInstance()
   .loadTextures()
   .then(() => {
     console.log('Textures loaded successfully');
+
+    //! Initialize and generate a chunk
+    const chunk = new Chunk();
+    chunk.generate();
+    const chunkRenderer = new ChunkRenderer(chunk);
+    chunkRenderer.render();
+
+    // Initialize the game
+    //TODO: All these will be handled inside World class
+    // For now, we just create a Game instance with the chunk
+    // This will also handle the scene, camera, and renderer setup
+    const game = new Game(chunk);
+
+    game.scene.add(chunkRenderer);
+
+    // Start the game loop
+    game.start();
+
+    createGUI(chunk, chunkRenderer);
   });
-
-//! Initialize and generate a chunk
-const chunk = new Chunk();
-chunk.generate();
-const chunkRenderer = new ChunkRenderer(chunk);
-chunkRenderer.render();
-
-// Initialize the game
-//TODO: All these will be handled inside World class
-// For now, we just create a Game instance with the chunk
-// This will also handle the scene, camera, and renderer setup
-const game = new Game(chunk);
-
-game.scene.add(chunkRenderer);
-
-// Start the game loop
-game.start();
-
-createGUI(chunk, chunkRenderer);
